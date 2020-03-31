e9facded9572fa191d7fad9fae3da7ba3808fbc5
Chiara Deluca <cdeluca@css.edu>
2020-03-31 14:38:10 -0500

    Assignment 10

---
+++ app/src/main/java/com/example/assignment10_chiara/Comment.java
@@ -0,0 +1,33 @@
+package com.example.assignment10_chiara;
+
+public class Comment {
+    private long id;
+    private String comment;
+    private String rating;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public String getRating() { return rating; }
+
+    public void setRating(String rating) { this.rating = rating; }
+
+    // Will be used by the ArrayAdapter in the ListView
+    @Override
+    public String toString() {
+        return "COMMENT{" + "id=" + id + "\'" + ", comment=" + comment + "rating=" + rating + "}";
+    }
+}
---
+++ app/src/main/java/com/example/assignment10_chiara/MainActivity.java
@@ -0,0 +1,76 @@
+package com.example.assignment10_chiara;
+
+import java.util.List;
+import java.util.Random;
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends ListActivity {
+    private CommentsDataSource datasource;
+    TextView textViewRate;
+    EditText editText;
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editText  = (EditText) findViewById(R.id.editText);
+        textViewRate = (TextView) findViewById(R.id.textViewRate);
+
+        datasource = new CommentsDataSource(this);
+        datasource.open();
+
+        List<Comment> values = datasource.getAllComments();
+        // use the SimpleCursorAdapter to show the
+        // elements in a ListView
+        ArrayAdapter<Comment> adapter = new ArrayAdapter<Comment>(this,
+                android.R.layout.simple_list_item_1, values);
+        setListAdapter(adapter);
+
+    }
+
+    // Will be called via the onClick attribute
+    // of the buttons in main.xml
+    public void onClick(View view) {
+        @SuppressWarnings("unchecked")
+        ArrayAdapter<Comment> adapter = (ArrayAdapter<Comment>) getListAdapter();
+        Comment comment = null;
+        switch (view.getId()) {
+            case R.id.add:
+                String[] comments = new String[] { "Cool", "Very nice", "Hate it" };
+                String[] rating = new String[] {"1", "2", "3", "4", "5"};
+                int nextInt = new Random().nextInt(3);
+                // save the new comment to the database
+                comment = datasource.createComment(comments[nextInt], rating[nextInt]);
+                adapter.add(comment);
+                break;
+            case R.id.delete:
+                if (getListAdapter().getCount() > 0) {
+                    comment = (Comment) getListAdapter().getItem(0);
+                    datasource.deleteComment(comment);
+                    adapter.remove(comment);
+                }
+                break;
+        }
+        adapter.notifyDataSetChanged();
+    }
+
+    @Override
+    protected void onResume() {
+        datasource.open();
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        datasource.close();
+        super.onPause();
+    }
+
+}
